01510c864c8fa52a97dc6a02b799aa0585b34f6e
Jramos <106244083+JRam0s@users.noreply.github.com>
2023-08-23 13:52:21 -0300

    Add files via upload

---
+++ JVzin/app/traits/Monitor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\traits;
+
+trait monitor
+{
+    public function monitor($returnAll = true)
+    {
+        try {
+            $query = $this->connection->query("SELECT DISTINCT turma, (SELECT COUNT(turma) FROM students WHERE turma=S.turma) AS total FROM {$this->table} S GROUP BY turma");
+                return $returnAll ? $query->fetchAll() : $query->fetch();
+        } catch (PDOException $e) {
+            var_dump($e->getMessage());
+        }
+    }
+}
